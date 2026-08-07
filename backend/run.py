6137ce86e65a7ec1d7c0280e4e5b6e2f4e9ad828
dolly-bit <dolly-bit@users.noreply.github.com>
2026-08-07 14:53:56 +0530

    Update run

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,10 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=False
     )
